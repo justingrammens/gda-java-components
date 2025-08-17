cef65a893b40311524f5836278faacd346d34eeb
63560596+labbenchstudios@users.noreply.github.com <63560596+labbenchstudios@users.noreply.github.com>
2025-08-16 23:41:28 -0400

    Updated IDE configs  - VS Code settings.json and launch.json  - Eclipse properties Updated lib dep versions (pom.xml) Added command line arg for config file Moved test classes around to match CDA  - Removed part01 - part04  - Added unit and integration sub-dirs README still needs to be updated

--- src/main/java/programmingtheiot/gda/app/GatewayDeviceApp.java
+++ src/main/java/programmingtheiot/gda/app/GatewayDeviceApp.java
@@ -11,6 +11,13 @@
 
 package programmingtheiot.gda.app;
 
+import org.apache.commons.cli.*;
+
+import programmingtheiot.common.ConfigConst;
+import programmingtheiot.common.ConfigUtil;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -29,21 +36,20 @@ public class GatewayDeviceApp
 	
 	// private var's
 	
-	
+	private String configFile = ConfigConst.DEFAULT_CONFIG_FILE_NAME;
+
 	// constructors
 	
 	/**
-	 * Constructor.
+	 * Default.
 	 * 
-	 * @param args
+	 * @param configFile
 	 */
-	public GatewayDeviceApp(String[] args)
+	public GatewayDeviceApp()
 	{
 		super();
 		
 		_Logger.info("Initializing GDA...");
-		
-		parseArgs(args);
 	}
 	
 	
@@ -56,17 +62,78 @@ public class GatewayDeviceApp
 	 */
 	public static void main(String[] args)
 	{
-		GatewayDeviceApp gwApp = new GatewayDeviceApp(args);
+		Map<String, String> argMap = parseArgs(args);
+
+		if (argMap.containsKey(ConfigConst.CONFIG_FILE_KEY)) {
+			System.setProperty(ConfigConst.CONFIG_FILE_KEY, argMap.get(ConfigConst.CONFIG_FILE_KEY));
+		}
+
+		GatewayDeviceApp gwApp = new GatewayDeviceApp();
 		
 		gwApp.startApp();
 		
-		try {
-			Thread.sleep(DEFAULT_TEST_RUNTIME);
-		} catch (InterruptedException e) {
-			// ignore
-		}
+		boolean runForever =
+			ConfigUtil.getInstance().getBoolean(ConfigConst.GATEWAY_DEVICE, ConfigConst.ENABLE_RUN_FOREVER_KEY);
 		
-		gwApp.stopApp(0);
+		if (runForever) {
+			try {
+				while (true) {
+					Thread.sleep(2000L);
+				}
+			} catch (InterruptedException e) {
+				// ignore
+			}
+			
+			gwApp.stopApp(0);
+		} else {
+			try {
+				Thread.sleep(DEFAULT_TEST_RUNTIME);
+			} catch (InterruptedException e) {
+				// ignore
+			}
+			
+			gwApp.stopApp(0);
+		}
+	}
+	
+	/**
+	 * Parse any arguments passed in on app startup.
+	 * <p>
+	 * This method should be written to check if any valid command line args are provided,
+	 * including the name of the config file. Once parsed, call {@link #initConfig(String)}
+	 * with the name of the config file, or null if the default should be used.
+	 * <p>
+	 * If any command line args conflict with the config file, the config file
+	 * in-memory content should be overridden with the command line argument(s).
+	 * 
+	 * @param args The non-null and non-empty args array.
+	 */
+	private static Map<String, String> parseArgs(String[] args)
+	{
+		// store command line values in a map
+		Map<String, String> argMap = new HashMap<String, String>();
+		
+		if (args != null && args.length > 0)  {
+			// create the parser and options - only need one for now ("c" for config file)
+			CommandLineParser parser = new DefaultParser();
+			Options options = new Options();
+
+			options.addOption("c", true, "The relative or absolute path of the config file.");
+
+			try {
+				CommandLine cmdLineArgs = parser.parse(options, args);
+
+				if (cmdLineArgs.hasOption("c")) {
+					argMap.put(ConfigConst.CONFIG_FILE_KEY, cmdLineArgs.getOptionValue("c"));
+				} else {
+					_Logger.info("No custom config file specified. Using default.");
+				}
+			} catch (ParseException e) {
+				_Logger.warning("Failed to parse command line args. Ignoring - using defaults.");
+			}
+		}
+
+		return argMap;
 	}
 	
 	
@@ -114,51 +181,5 @@ public class GatewayDeviceApp
 	
 	// private methods
 	
-	/**
-	 * Load the config file.
-	 * 
-	 * NOTE: This will be added later.
-	 * 
-	 * @param configFile The name of the config file to load.
-	 */
-	private void initConfig(String configFile)
-	{
-		_Logger.log(Level.INFO, "Attempting to load configuration: {0}", (configFile != null ? configFile : "Default."));
-		
-		// TODO: Your code here
-	}
-	
-	/**
-	 * Parse any arguments passed in on app startup.
-	 * <p>
-	 * This method should be written to check if any valid command line args are provided,
-	 * including the name of the config file. Once parsed, call {@link #initConfig(String)}
-	 * with the name of the config file, or null if the default should be used.
-	 * <p>
-	 * If any command line args conflict with the config file, the config file
-	 * in-memory content should be overridden with the command line argument(s).
-	 * 
-	 * @param args The non-null and non-empty args array.
-	 */
-	private void parseArgs(String[] args)
-	{
-		String configFile = null;
-		
-		if (args != null) {
-			_Logger.log(Level.INFO, "Parsing {0} command line args.", args.length);
-			
-			for (String arg : args) {
-				if (arg != null) {
-					arg = arg.trim();
-					
-					// TODO: Your code here
-				}
-			}
-		} else {
-			_Logger.info("No command line args to parse.");
-		}
-		
-		initConfig(configFile);
-	}
 
 }
